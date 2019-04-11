756aa7afd5de0be7f18ff40e44265dca23118b0b
Park <winkknd@naver.com>
2019-04-12 01:52:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const app = express();
 
 const PORT = 4000;
 
+function handleHome() {
+  res.send("hello from home");
+}
+
+function handleProfile() {
+  res.send("profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handlePrifile);
+
 function handleListening() {
   console.log(`Listening on http://localhost:${PORT}`);
 }
