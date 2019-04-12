6a46aaea43c08072b113fc97c9d165d5b4d8e97f
Park <winkknd@naver.com>
2019-04-13 02:12:23 +0900

    .

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
 
 app.use(routes.home, globalRouter);
--- init.js
+++ init.js
@@ -3,6 +3,6 @@ import app from "./app";
 const PORT = 4000;
 
 const handleListening = () =>
-  console.log(`🌈Listening on: http://localhost:${PORT}`);
+  console.log(`🌈  Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
