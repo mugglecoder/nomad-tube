5e4cb224f4f2df87ea39f32cc01899cd325f5fca
Park <winkknd@naver.com>
2019-04-14 01:58:18 +0900

    .

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 332432,
+    title: "Video awesome",
+    description: "kinchi",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "nico",
+      email: "winkknd@naver.com"
+    }
+  },
+  {
+    id: 3324231332,
+    title: "Video 222222",
+    description: "kinchi122222",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "nico",
+      email: "winkknd@naver.com"
+    }
+  },
+  {
+    id: 332312432,
+    title: "Video 333333",
+    description: "kinchi3333333",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "nico",
+      email: "winkknd@naver.com"
+    }
+  },
+  {
+    id: 33243232,
+    title: "Video 4444444",
+    description: "kinchi4444",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "nico",
+      email: "winkknd@naver.com"
+    }
+  }
+];
