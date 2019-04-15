4cd5d247cabbd9837798424456dd92c052a0d0e3
Park <winkknd@naver.com>
2019-04-16 06:56:22 +0900

    .

--- db.js
+++ db.js
@@ -1,54 +1,14 @@
-export const videos = [
-  {
-    id: 332432,
-    title: "Video awesome",
-    description: "kinchi",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "nico",
-      email: "winkknd@naver.com"
-    }
-  },
-  {
-    id: 3324231332,
-    title: "Video 222222",
-    description: "kinchi122222",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "nico",
-      email: "winkknd@naver.com"
-    }
-  },
-  {
-    id: 332312432,
-    title: "Video 333333",
-    description: "kinchi3333333",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "nico",
-      email: "winkknd@naver.com"
-    }
-  },
-  {
-    id: 33243232,
-    title: "Video 4444444",
-    description: "kinchi4444",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "nico",
-      email: "winkknd@naver.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/nomad-TUBE", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("🌈  Connected to DB");
+const handleError = error => console.log(`❌   DB : ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
